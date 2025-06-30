877899f5ec8f9bde22526ef7ca23e3aae47fcf74
Boglarka Molnar <boglarka_m@yahoo.com>
2025-06-30 14:52:25 +0300

    hover effects and error messages

---
+++ conference-ticket-generator/script.js
@@ -0,0 +1,86 @@
+const dropZone = document.getElementById("dropZone");
+const fileInput = document.getElementById("fileInput");
+
+dropZone.addEventListener("click", () => {
+	fileInput.click();
+});
+
+fileInput.addEventListener("change", () => {
+	if (fileInput.files.length > 0) {
+		const file = fileInput.files[0];
+
+		dropZone.innerHTML = `<p>${file.name}</p>`;
+
+		if (file.type.startsWith("image/")) {
+			const reader = new FileReader();
+			reader.onload = function (e) {
+				dropZone.innerHTML = `<img src="${e.target.result}" alt="Preview" style="width: 50px; border-radius: 10px; border: 0.1px solid hsl(252, 6%, 83%); " />`;
+			};
+			reader.readAsDataURL(file);
+		}
+	}
+});
+
+const button = document.querySelector(".btn");
+const contents = document.querySelectorAll(".content");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const githubInput = document.getElementById("github-user");
+const info = document.querySelectorAll(".info p");
+
+const nameError = nameInput.nextElementSibling;
+const emailError = emailInput.nextElementSibling;
+const githubError = githubInput.nextElementSibling;
+const fileError = fileInput.nextElementSibling;
+
+button.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	let isValid = true;
+
+	// Reset errors
+	[nameInput, emailInput, githubInput, fileInput].forEach((input) => {
+		input.classList.remove("error");
+	});
+	[nameError, emailError, githubError].forEach((span) => {
+		span.textContent = "";
+	});
+
+	// Validate Name
+	if (nameInput.value.trim() === "") {
+		isValid = false;
+		nameInput.classList.add("error");
+		nameError.textContent = "Full Name is required.";
+	}
+
+	// Validate Email
+	if (!validateEmail(emailInput.value.trim())) {
+		isValid = false;
+		emailInput.classList.add("error");
+		emailError.textContent = "Enter a valid email address.";
+	}
+
+	// Validate GitHub
+	if (githubInput.value.trim() === "") {
+		isValid = false;
+		githubInput.classList.add("error");
+		githubError.textContent = "GitHub Username is required.";
+	}
+
+	// Validate File Upload
+	if (fileInput.files.length === 0) {
+		isValid = false;
+		info[0].classList.add("error");
+	}
+	// If all valid, show ticket
+	if (isValid) {
+		contents.forEach((section) => {
+			section.classList.toggle("hidden");
+		});
+	}
+});
+
+function validateEmail(email) {
+	const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return re.test(email);
+}
